b9b5202cf4c24baa73f154e17f438eb90f703194
Vince Klyn <vince_klyn@yahoo.com>
2019-07-19 12:46:08 +0800

    javascript

---
+++ script.js
@@ -0,0 +1,79 @@
+function addition(){
+    var x = document.getElementById('num1').value;
+    var y = document.getElementById('num2').value;
+
+    if (isNaN(num1)  || isNaN(num2)){
+        alert("Must enter a number.");
+    }
+    else if (num1 == "" || num2 == ""){
+        alert("Invalid Input");
+    }
+    else{
+        num1 = Number(num1);
+        num2 = Number(num2);
+
+        var sum = num1 + num2;
+
+        alert("Sum is : " + sum);
+    }
+}
+
+function subtraction(){
+    var x = document.getElementById('num1').value;
+    var y = document.getElementById('num2').value;
+
+    if (isNaN(num1)  || isNaN(num2)){
+        alert("Must enter a number.");
+    }
+    else if (num1 == "" || num2 == ""){
+        alert("Invalid Input");
+    }
+    else{
+        num1 = Number(num1);
+        num2 = Number(num2);
+
+        var sum = num1 + num2;
+
+        alert("Sum is : " + sum);
+    }
+}
+
+function multiplication(){
+    var x = document.getElementById('num1').value;
+    var y = document.getElementById('num2').value;
+
+    if (isNaN(num1)  || isNaN(num2)){
+        alert("Must enter a number.");
+    }
+    else if (num1 == "" || num2 == ""){
+        alert("Invalid Input");
+    }
+    else{
+        num1 = Number(num1);
+        num2 = Number(num2);
+
+        var sum = num1 + num2;
+
+        alert("Sum is : " + sum);
+    }
+}
+
+function division(){
+    var x = document.getElementById('num1').value;
+    var y = document.getElementById('num2').value;
+
+    if (isNaN(num1)  || isNaN(num2)){
+        alert("Must enter a number.");
+    }
+    else if (num1 == "" || num2 == ""){
+        alert("Invalid Input");
+    }
+    else{
+        num1 = Number(num1);
+        num2 = Number(num2);
+
+        var sum = num1 + num2;
+
+        alert("Sum is : " + sum);
+    }
+}
